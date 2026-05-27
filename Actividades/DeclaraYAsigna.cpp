d945e467f90ab757f7babd2c010dab83e16ef3f7
Maria Fernanda Herrera Ramirez <mherreraramirez5002@gmail.com>
2026-05-27 12:43:44 -0600

    Added Problema 2 activity

---
+++ Actividades/DeclaraYAsigna.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Declaracion de variables
+    int n1 = 10;
+    int n2 = 20;
+
+    // Suma de variables
+    int suma = n1 + n2;
+
+    // Mostrar resultado
+    cout << "La suma de " << n1 << " + " << n2;
+    cout << " es: " << suma << endl;
+
+    return 0;
+}

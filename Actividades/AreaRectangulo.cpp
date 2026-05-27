6a102ea43a0cd838b993ff5f9a424dab472636e6
Maria Fernanda Herrera Ramirez <mherreraramirez5002@gmail.com>
2026-05-27 12:48:54 -0600

    Added rectangle area activity

---
+++ Actividades/AreaRectangulo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Declaracion de variables
+    float base = 5.5;
+    float altura = 3.0;
+    float area;
+
+    // Calculo del area
+    area = base * altura;
+
+    // Mostrar resultado
+    cout << "Base: " << base << endl;
+    cout << "Altura: " << altura << endl;
+    cout << "Area del rectangulo: " << area << endl;
+
+    return 0;
+}

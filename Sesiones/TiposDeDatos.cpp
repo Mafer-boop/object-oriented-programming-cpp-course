2302e3b46e0a70aa9db4468827e4ac5ffdbb3f36
Maria Fernanda Herrera Ramirez <mherreraramirez5002@gmail.com>
2026-05-27 11:13:11 -0600

    Added data types example in C++

---
+++ Sesiones/TiposDeDatos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "\nDeclaro variables..." << endl;
+    cout << "Uso tipos int, char, float y double" << endl;
+
+    int entero1 = 5, entero2;
+    char letraA = 'a', letraB = 'b', digito0 = '0';
+    float flotante = 3.0;
+    double doble = 55.0;
+    bool booleano = true;
+
+    cout << "Y luego imprimo aquellas a las que ya les asigne valor: \n";
+
+    cout << entero1 << " " << letraA << " " << digito0 << endl;
+    cout << flotante << " " << doble << " " << booleano << "\n";
+
+    cout << "\nTermine \nFIN\n";
+
+    return 0;
+}

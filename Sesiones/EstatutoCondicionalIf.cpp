838ecfd8a9b1dcde1eb938a7d63a6761ec1caf3a
Maria Fernanda Herrera Ramirez <mherreraramirez5002@gmail.com>
2026-05-27 11:41:05 -0600

    Added if conditional example

---
+++ Sesiones/EstatutoCondicionalIf.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    bool esDomingo = true;
+
+    if(esDomingo) {
+        cout << "Feliz domingo!!" << endl;
+    }
+
+    return 0;
+}

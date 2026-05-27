7f8d28edd8c0c420c651109db8bbbcde90e5c266
Maria Fernanda Herrera Ramirez <mherreraramirez5002@gmail.com>
2026-05-27 10:07:28 -0600

    Sesion1 de entrada y salida en C++

---
+++ Sesiones/Sesion1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    int numero;
+    string palabra;
+
+    cout << "Dame un numero: ";
+    cin >> numero;
+
+    cout << "Dame una palabra: ";
+    cin >> palabra;
+
+    cout << "El numero es " << numero
+         << " y la palabra " << palabra << endl;
+
+    return 0;
+}
